363ce1e19b3ede8fd7e653aa795e54c688955ed2
StephanieML <smlind13@g.holycross.edu>
2014-11-10 14:39:26 -0500

    set up concordion

---
+++ markdown/writing/episodes/ProclusTest.java
@@ -0,0 +1,14 @@
+package writing.episodes;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+import org.concordion.integration.junit3.ConcordionTestCase;
+
+/* Run this class as a JUnit test. */
+
+public class ProclusTest extends ConcordionTestCase {
+    
+    public String getPassageBegin(String urn) {
+        return ("Never looked anything up.");
+    }
+}
