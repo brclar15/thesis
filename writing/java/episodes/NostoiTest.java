692760f6046b836cba3e6c6ac139461a0c25a1a4
Brian Clark <brclar15@g.holycross.edu>
2014-11-30 23:16:28 -0500

    Create NostoiTest.java
    added file

---
+++ writing/java/episodes/NostoiTest.java
@@ -0,0 +1,14 @@
+package nostoi;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+import org.concordion.integration.junit3.ConcordionTestCase;
+
+/* Run this class as a JUnit test. */
+
+public class NostoiTest extends ConcordionTestCase {
+    
+    public String getPassageBegin(String urn) {
+        return ("Never looked anything up for urn " + urn);
+    }
+}
